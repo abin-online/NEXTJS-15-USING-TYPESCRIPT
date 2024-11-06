f7a791bdef01afd736f167b12b7f34da5af1705e
Abin Babu <abinbabuonline@gmail.com>
2024-11-06 09:13:19 +0530

    Component setting

--- src/app/component.tsx/Counter.jsx
+++ src/app/component.tsx/Counter.jsx
@@ -1,8 +1,11 @@
 "use client"
+
 import { useState } from "react"
 
 const Counter = () => {
     const [count , setCount] = useState(0)
+    
+    console.log('Counter updates')
   return (
     <>
         <button onClick={()=> {setCount((prevCount)=>prevCount + 1)}}>clicked {count} times</button>
